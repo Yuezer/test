16be5caa7d88f99bac85c9679e383ca0cf12980c
Yuezer_Mrs <824321853@qq.com>
2018-04-01 17:11:05 +0800

    fistsubmit第三次更新

--- test_all.py
+++ test_all.py
@@ -14,8 +14,6 @@ class Test_allure:
     @allure.step("我是测试步骤02")       #添加测试步骤
     def test_al2(self):
         allure.attach('描述', '我是测试第二步的描述')  # 添加测试描述
-        assert 1
+        assert 0
 
-    @allure.step("我是测试步骤03")       #添加测试步骤
-    def test_al3(self):
-        assert 1
+ 
