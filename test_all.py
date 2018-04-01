92c6026bebc7885661e2669530101b33d441ae67
Yuezer_Mrs <824321853@qq.com>
2018-04-01 17:19:59 +0800

    fistsubmit01

--- test_all.py
+++ test_all.py
@@ -14,6 +14,5 @@ class Test_allure:
     @allure.step("我是测试步骤02")       #添加测试步骤
     def test_al2(self):
         allure.attach('描述', '我是测试第二步的描述')  # 添加测试描述
-        assert 0
+        assert 1
 
- 
